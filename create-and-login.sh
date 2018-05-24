f4e16014d7f94913aa85b39ca1e2b3d7d341fa6c
Andrew Morgan <andrewm@matrix.org>
2018-05-24 14:40:35 +0100

    Remove dependency on resty

--- create-and-login.sh
+++ create-and-login.sh
@@ -1,12 +1,9 @@
 username="testing-$(pwgen 4 1)"
 password="testing123"
 
-resty "http://127.0.0.1:8008"
-
-reg_res="$(POST '/_matrix/client/r0/register' '{"username": "'$username'", "password": "'$password'"}' 2>&1)"
-session="$(echo $reg_res | jq .session)"
-reg_res2="$(POST '/_matrix/client/r0/register' '{"username": "'$username'", "password": "'$password'", "auth": {"type": "m.login.dummy", "session": '$session'}}' 2>&1)"
-echo $reg_res2
+reg_res="$(curl -sL --data-binary \{\"username\":\ \"$username\",\ \"password\":\ \"$password\"\} -X POST "http://127.0.0.1:8008/_matrix/client/r0/register")"
+session="$(echo $reg_res | jq -r .session)"
+reg_res2="$(curl -sL --data-binary \{\"username\":\ \"$username\",\ \"password\":\ \"$password\"\,\ \"auth\":\ \{\"type\":\ \"m.login.dummy\",\ \"session\":\ \"$session\"\}\} -X POST "http://127.0.0.1:8008/_matrix/client/r0/register")"
 
 # -r - Don't print with surrounding quotes
 user_id=$(echo "$reg_res2" | jq -r .user_id)
@@ -23,4 +20,3 @@ echo "\$USERNAME: $user_id"
 echo "\$ACCESS_TOKEN: $access_token"
 echo "\$DEVICE_ID: $device_id"
 echo "\$PASSWORD: $password"
-resty "http://127.0.0.1:8008" -H "Authorization: Bearer $ACCESS_TOKEN"
