eb72488eba63380f6b64980d78156cd8038a77a0
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2018-06-19 17:55:49 -0700

    Add appservice db to wipe script

--- wipe-dendrite-db.sh
+++ wipe-dendrite-db.sh
@@ -1,4 +1,4 @@
-for i in account device mediaapi syncapi roomserver serverkey federationsender publicroomsapi naffka; do
+for i in account device mediaapi syncapi roomserver serverkey federationsender publicroomsapi naffka appservice; do
   sudo -u postgres dropdb dendrite_$i
   sudo -u postgres createdb -O dendrite dendrite_$i
 done
