4b777e401026282bf3bb095232f216e2fc3445a6
Andrew Morgan <andrewm@matrix.org>
2018-07-16 13:26:52 +0100

    Add dummyas script, fix wipe script

---
+++ dummyas/README.md
@@ -0,0 +1,12 @@
+# Dummy Application Service
+
+Little python scripts that can act as an application service to interface with.
+
+Can be run with:
+
+```
+./server.py <port>
+```
+
+The unresponsive server is a script that accepts transactions, but does not
+respond to them. Useful for testing network packet drops.
---
+++ dummyas/unresponsive_server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+Very simple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("{}")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        json_body = json.loads(post_body)
+        print json.dumps(json_body, indent=2, sort_keys=False)
+        
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
